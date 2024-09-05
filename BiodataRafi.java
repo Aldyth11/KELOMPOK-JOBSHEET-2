8d841ad2ba69d4ef3a3d9251e816515601accbe3
Rafi Abyantara Pratama <rafiabyantarapratama@gmail.com>
2024-09-05 12:41:15 +0700

    Biodata Rafi

---
+++ BiodataRafi.java
@@ -0,0 +1,16 @@
+public class BiodataRafi {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : Rafi Abyantara Pratama");
+        System.out.println("NIM      : 244107020126");
+        System.out.println("Kelas    : TI 1D");
+        System.out.println("No Absen : 24");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : JL Kesumba No 7, Lowokwaru,Jatimulyo,Kota Malang");
+        System.out.println("No HP    : 085850538320");
+        System.out.println("Hobi     : Berenang");
+        
+    }
+    }
