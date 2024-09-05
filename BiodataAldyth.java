dacac328723fd2e056f2a4d60fc5e60ccbf3cbe5
Aldyth11 <aldyth.r.f@gmail.com>
2024-09-05 12:33:38 +0700

    Biodata Aldyth

---
+++ BiodataAldyth.java
@@ -0,0 +1,16 @@
+public class BiodataAldyth {
+    public static void main(String[] args) {
+        System.out.println("---------------------------------------------------------");
+        System.out.println("-- Data Mahasiswa Jurusan Teknologi Informasi POLINEMA --");
+        System.out.println("---------------------------------------------------------");
+        System.out.println("Nama     : M. Aldyth Rafiansyah Fauzi");
+        System.out.println("NIM      : 244107020179");
+        System.out.println("Kelas    : TI 1D");
+        System.out.println("No Absen : 15");
+        System.out.println("Prodi    : Teknik Informatika");
+        System.out.println("Alamat   : JL Grindulu No 135");
+        System.out.println("No HP    : 085234333100");
+        System.out.println("Hobi     : Bulutangkis");
+        
+    }
+}
